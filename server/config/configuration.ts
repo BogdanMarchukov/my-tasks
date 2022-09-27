75af00ebd1616e778887e8d210734f013b6541a7
bogdan <bogdan_info@mail.ru>
2022-09-27 20:17:38 +0300

    init pg

---
+++ server/config/configuration.ts
@@ -0,0 +1,39 @@
+export interface CliType {
+  migrationsDir: string;
+}
+
+export interface DbType {
+  type: string;
+  host: string;
+  port: number;
+  username: string;
+  password: string;
+  database: string;
+  entities: string[];
+  migrationsTableName: string;
+  migrations: string[];
+  cli: CliType;
+  autoLoadEntities;
+}
+
+interface ConfigType {
+  db: DbType;
+}
+
+export default (): ConfigType => ({
+  db: {
+    type: 'postgres',
+    host: process.env.POSTGRES_HOST || '127.0.0.1',
+    port: parseInt(process.env.POSTGRES_PORT) || 5432,
+    username: process.env.POSTGRES_USER || 'postgres',
+    password: process.env.POSTGRES_PASSWORD || 'mysecretpassword',
+    database: process.env.POSTGRES_DATABASE || 'postgres',
+    entities: ['dist/**/*.entity.js'],
+    migrationsTableName: 'migration',
+    migrations: ['dist/migrations/*.js'],
+    cli: {
+      migrationsDir: 'src/migrations',
+    },
+    autoLoadEntities: true,
+  },
+});
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import configuration from '../config/configuration';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => configService.get('db'),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AppModule {}
